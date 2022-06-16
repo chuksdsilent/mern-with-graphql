5ba0e85ea737c5dcc35deecca0d0f2f1b5f7708e
Oshaba Samson <chuksdsilent@gmail.com>
2022-06-16 12:09:11 +0100

    added update

--- schema/schema.js
+++ schema/schema.js
@@ -135,6 +135,16 @@ const mutation = new GraphQLObjectType({
         return project.save();
       },
     },
+
+    deleteProject: {
+      type: ProjectType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        return Project.findByIdAndRemove(args.id);
+      },
+    },
   },
 });
 
