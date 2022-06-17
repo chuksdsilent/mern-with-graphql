4a67d2fd4a0f54e8d3726bf9a4b9ab60d3d4cb34
Oshaba Samson <chuksdsilent@gmail.com>
2022-06-17 05:52:54 +0100

    Added get clients

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ require("dotenv").config();
 const schema = require("./schema/schema");
 const { graphqlHTTP } = require("express-graphql");
 const PORT = process.env.PORT || 5000;
-
+const cors = require("cors");
 const app = express();
 
 connectDB();
+
+app.use(cors());
 
 app.use(
   "/graphql",
