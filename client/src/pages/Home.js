439a886008200f406a777c2495ed0aac34e8a4ff
Oshaba Samson <chuksdsilent@gmail.com>
2022-06-21 00:07:12 +0100

    added AddProjectModal

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,17 @@ import React from "react";
 import { Project } from "../components/Project";
 import AddClientModal from "../components/AddClientModal";
 import { Clients } from "../components/Clients";
+import AddProjectModal from "../components/AddProjectModal";
 
 export const Home = () => {
   return (
     <>
-      <AddClientModal />
+      <div className="d-flex gap-3 mb-4">
+        <AddProjectModal />
+        <AddClientModal />
+      </div>
       <Project />
+      <hr />
       <Clients />
     </>
   );
